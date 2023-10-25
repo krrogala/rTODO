aab6a710a8063a3ed8bb654873ee060f78b51c71
Kevin Rogala <krrogala@proton.me>
2023-10-25 13:40:56 -0500

    Implement Sqlalchemy

--- services/todo_api/fastapi_todo/main.py
+++ services/todo_api/fastapi_todo/main.py
@@ -1,10 +1,31 @@
-# fastapi_todo/main.py
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import crud, models, schemas, database
 
-from fastapi import FastAPI
+models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
+# Dependency to get the database session
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to rTODO!"}
+@app.post("/todos/", response_model=schemas.Todo)
+def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+    return crud.create_todo(db=db, todo=todo)
+
+@app.get("/todos/", response_model=list[schemas.Todo])
+def read_todos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    todos = crud.get_todos(db, skip=skip, limit=limit)
+    return todos
+
+@app.get("/todos/{todo_id}", response_model=schemas.Todo)
+def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = crud.get_todo(db, todo_id=todo_id)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_todo
