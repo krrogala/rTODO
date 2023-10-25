24ada6ef03b289bc63ee6d1226e80924ce83c139
Kevin Rogala <krrogala@proton.me>
2023-10-25 12:24:44 -0500

    Move to proper SQLAlchemy 2.0 implementation

--- services/todo_api/fastapi_todo/config.py
+++
@@ -1,3 +0,0 @@
-# fastapi_todo/config.py
-
-DATABASE_URL = "sqlite+aiosqlite:///./test.db"  # SQLite database path
--- services/todo_api/initialize_db.py
+++
@@ -1,8 +0,0 @@
-# services/todo_api/initialize_db.py
-
-import asyncio
-
-from fastapi_todo.database import init_db
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init_db())
